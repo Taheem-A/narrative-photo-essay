599ad9121f83570908e961fa764a8d6575346d53
Taheem Akter <taheem.akter@gmail.com>
2025-06-12 08:42:01 -0400

    Fix capitalization in backText for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const backTexts = [
   "Appropriation is not always intentional or obvious.",
   "The turtle was always there. What changed was me.",
   "Silence can lead to erasure.",
-  "reconciliation is more than just grieving the past."
+  "Reconciliation is more than just grieving the past."
 ];
 const imageUrls = [
   bg1,
